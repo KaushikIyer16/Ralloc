2724dc5ff46a4c9c08a423211458f469bfa3bc52
Sneha Kannan <kannansneha.1997@gmail.com>
2017-02-27 22:44:08 +0530

    View addition

---
+++ Project1/src/java/com/ralloc/beans/RoomBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ralloc.beans;
+
+/**
+ *
+ * @author kaushiknsiyer
+ */
+public class RoomBean {
+    int roomId;
+    int capacity;
+
+    public RoomBean(int roomId, int capacity) {
+        this.roomId = roomId;
+        this.capacity = capacity;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    
+    
+}
---
+++ Project1/src/java/com/ralloc/model/Subject.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ralloc.model;
+
+/**
+ *
+ * @author mahesh
+ */
+public class Subject {
+    String courseCode, name;
+    boolean isDeptElective, isClusterElective, isInstituteElective;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public boolean isIsDeptElective() {
+        return isDeptElective;
+    }
+
+    public void setIsDeptElective(boolean isDeptElective) {
+        this.isDeptElective = isDeptElective;
+    }
+
+    public boolean isIsClusterElective() {
+        return isClusterElective;
+    }
+
+    public void setIsClusterElective(boolean isClusterElective) {
+        this.isClusterElective = isClusterElective;
+    }
+
+    public boolean isIsInstituteElective() {
+        return isInstituteElective;
+    }
+
+    public void setIsInstituteElective(boolean isInstituteElective) {
+        this.isInstituteElective = isInstituteElective;
+    }
+    
+    
+}
