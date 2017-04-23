22c3d4266f2e1b1ebf44d0e48352635ea4e5879c
saurabhschheda <saurabh.s.chheda@gmail.com>
2017-04-24 01:16:38 +0530

    Added Picture to Document

--- RallocWeb/src/java/com/ralloc/controller/BFormDocument.java
+++ RallocWeb/src/java/com/ralloc/controller/BFormDocument.java
@@ -15,14 +15,18 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.sql.SQLException;
 import java.math.BigInteger;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlToken;
+import org.apache.poi.xwpf.usermodel.Document;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFPicture;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblPr;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
@@ -30,6 +34,10 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblWidth;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblBorders;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBorder;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STHeightRule;
+import org.openxmlformats.schemas.drawingml.x2006.main.CTNonVisualDrawingProps;
+import org.openxmlformats.schemas.drawingml.x2006.main.CTPositiveSize2D;
+import org.openxmlformats.schemas.drawingml.x2006.wordprocessingDrawing.CTInline;
+
 /**
  *
  * @author saurabh
@@ -47,6 +55,68 @@ public class BFormDocument {
       this.out = out;
       document = new XWPFDocument();
    }
+
+   public void createPicture(XWPFParagraph para, String blipId,int id, int width, int height) {
+      final int EMU = 9525;
+      width *= EMU;
+      height *= EMU;
+      //String blipId = getAllPictures().get(id).getPackageRelationship().getId();
+
+      XWPFRun run = para.createRun();
+      CTInline inline = run.getCTR().addNewDrawing().addNewInline();
+
+      String picXml = "" +
+         "<a:graphic xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\">" +
+         "   <a:graphicData uri=\"http://schemas.openxmlformats.org/drawingml/2006/picture\">" +
+         "      <pic:pic xmlns:pic=\"http://schemas.openxmlformats.org/drawingml/2006/picture\">" +
+         "         <pic:nvPicPr>" +
+         "            <pic:cNvPr id=\"" + id + "\" name=\"Generated\"/>" +
+         "            <pic:cNvPicPr/>" +
+         "         </pic:nvPicPr>" +
+         "         <pic:blipFill>" +
+         "            <a:blip r:embed=\"" + blipId + "\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\"/>" +
+         "            <a:stretch>" +
+         "               <a:fillRect/>" +
+         "            </a:stretch>" +
+         "         </pic:blipFill>" +
+         "         <pic:spPr>" +
+         "            <a:xfrm>" +
+         "               <a:off x=\"0\" y=\"0\"/>" +
+         "               <a:ext cx=\"" + width + "\" cy=\"" + height + "\"/>" +
+         "            </a:xfrm>" +
+         "            <a:prstGeom prst=\"rect\">" +
+         "               <a:avLst/>" +
+         "            </a:prstGeom>" +
+         "         </pic:spPr>" +
+         "      </pic:pic>" +
+         "   </a:graphicData>" +
+         "</a:graphic>";
+
+        //CTGraphicalObjectData graphicData = inline.addNewGraphic().addNewGraphicData();
+        XmlToken xmlToken = null;
+        try {
+           xmlToken = XmlToken.Factory.parse(picXml);
+        } catch(XmlException xe) {
+            xe.printStackTrace();
+        }
+        inline.set(xmlToken);
+        //graphicData.set(xmlToken);
+
+        inline.setDistT(0);
+        inline.setDistB(0);
+        inline.setDistL(0);
+        inline.setDistR(0);
+
+        CTPositiveSize2D extent = inline.addNewExtent();
+        extent.setCx(width);
+        extent.setCy(height);
+
+        CTNonVisualDrawingProps docPr = inline.addNewDocPr();
+        docPr.setId(id);
+        docPr.setName("Picture " + id);
+        docPr.setDescr("Generated");
+        run.addBreak();
+    }
 
    private void writeRun(XWPFParagraph para, String text, boolean breakLine) {
       XWPFRun run = para.createRun();
@@ -68,6 +138,17 @@ public class BFormDocument {
       XWPFParagraph heading = document.createParagraph();
       heading.setPageBreak(true);
       heading.setAlignment(ParagraphAlignment.CENTER);
+      try {
+         String blipID = document.addPictureData(new FileInputStream("/media/drgreat5/My Stuff/Projects/Ralloc/RallocWeb/web/images/BMS_LOGO_Print.png"), Document.PICTURE_TYPE_PNG);
+         createPicture(heading, blipID,document.getNextPicNameNumber(Document.PICTURE_TYPE_JPEG), 75, 75);
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (InvalidFormatException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+
       writeRun(heading, "B.M.S COLLEGE OF ENGINEERING(Autonomous Institution under VTU), BANGALORE - 560 019", true);
       writeRun(heading, "Attendance and Room Superintendent's Report", true);
       writeRun(heading, "B.E./B.Arch./M.B.A/M.C.A/M.Tech./Ph.D./M.Sc.(Res) _______ Semester Examination " + date.substring(3, 10), true);
